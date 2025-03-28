7c7c8ef37173f4f764d2668e58c426e31c0d84ea
Pedro Gruz <115219064+pgomez-r@users.noreply.github.com>
2025-03-28 19:06:31 +0000

    maria_ports_fixed

--- srcs/requirements/wordpress/conf/wp-init_state.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /var/www/html/wp-content/initialized ]; then
-  cp -r /initial-state/wp-content/* /var/www/html/wp-content/
-  touch /var/www/html/wp-content/initialized
-fi
